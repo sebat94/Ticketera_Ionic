b3283ac22bfbceb51fe50aef6856bc066888d5ca
sebat94 <se_bat_94@hotmail.com>
2020-01-30 23:17:45 +0100

    Removed wrong folder

--- Ionic/ionic-events/src/constants/static-content.ts
+++
@@ -1,7 +0,0 @@
-const IMAGES_EVENT_DIRECTORY: string = 'http://192.168.1.11:8080/public/images/event/';
-const IMAGES_USER_DIRECTORY: string = 'http://192.168.1.11:8080/public/images/user/';
-
-export {
-    IMAGES_EVENT_DIRECTORY,
-    IMAGES_USER_DIRECTORY
-}
